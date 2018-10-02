abf57ebaf0e6c8da59f30517bccb765b7efa2137
Nuno Filipe Coimbra <nuno.coimbra@alsglobal.com>
2018-10-02 17:19:58 +0100

    Final test work

--- GeolocationTest/GeolocationTest/GeolocationTest.Android/Properties/AssemblyInfo.cs
+++ GeolocationTest/GeolocationTest/GeolocationTest.Android/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Android.App;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GeolocationTest.Android")]
@@ -19,11 +19,11 @@ using Android.App;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
@@ -32,3 +32,8 @@ using Android.App;
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
+[assembly: UsesFeature("android.hardware.location", Required = false)]
+[assembly: UsesFeature("android.hardware.location.gps", Required = false)]
+[assembly: UsesFeature("android.hardware.location.network", Required = false)]
--- GeolocationTest/GeolocationTest/GeolocationTest/MainPage.xaml.cs
+++ GeolocationTest/GeolocationTest/GeolocationTest/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace GeolocationTest
 {
@@ -14,23 +15,36 @@ namespace GeolocationTest
             InitializeComponent();
         }
 
-        private async void Button_Clicked(object sender, EventArgs e)
+        public async void Button_Clicked(object sender, EventArgs e)
         {
-            var request = new Xamarin.Essentials.GeolocationRequest(Xamarin.Essentials.GeolocationAccuracy.Best, TimeSpan.FromSeconds(20));
-            var request1 = new Xamarin.Essentials.GeolocationRequest(Xamarin.Essentials.GeolocationAccuracy.Best);
-            var request2 = new Xamarin.Essentials.GeolocationRequest();
+            try
+            {
+                var request = new GeolocationRequest(Xamarin.Essentials.GeolocationAccuracy.Best, TimeSpan.FromSeconds(10));
+                var request1 = new GeolocationRequest(Xamarin.Essentials.GeolocationAccuracy.Lowest, TimeSpan.FromSeconds(10));
 
-            var location = await Xamarin.Essentials.Geolocation.GetLastKnownLocationAsync();
-            var location1 = await Xamarin.Essentials.Geolocation.GetLocationAsync(request);
-            var location2 = await Xamarin.Essentials.Geolocation.GetLocationAsync(request1);
-            var location3 = await Xamarin.Essentials.Geolocation.GetLocationAsync(request2);
-            var location4 = await Xamarin.Essentials.Geolocation.GetLocationAsync();
+                var location = await Geolocation.GetLastKnownLocationAsync();
+                var location1 = await Geolocation.GetLocationAsync(request);
+                var location2 = await Geolocation.GetLocationAsync(request1);
 
-            lblLocation.Text = $"Lat: {location.Latitude} .-. Long: {location.Longitude}";
-            lblLocation1.Text = $"Lat: {location1.Latitude} .-. Long: {location1.Longitude}";
-            lblLocation2.Text = $"Lat: {location2.Latitude} .-. Long: {location2.Longitude}";
-            lblLocation3.Text = $"Lat: {location3.Latitude} .-. Long: {location3.Longitude}";
-            lblLocation4.Text = $"Lat: {location4.Latitude} .-. Long: {location4.Longitude}";
+                    lblLocation.Text = $"Lat: {location.Latitude} .-. Long: {location.Longitude}";
+                    lblLocation1.Text = $"Lat: {location1.Latitude} .-. Long: {location1.Longitude}";
+                    lblLocation2.Text = $"Lat: {location2.Latitude} .-. Long: {location2.Longitude}";
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+                throw fnsEx;
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+                throw pEx;
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+                throw ex;
+            }
         }
     }
 }
